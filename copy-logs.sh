2edfaaad4327c9170bdce757cb7d57ea96b26052
Enis Soztutar <enis@apache.org>
2015-05-12 11:31:52 -0700

    copy logs

--- copy-logs.sh
+++ copy-logs.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+if [ "$#" -eq "0" ] ; then
+  echo "usage: $0 <target_path> "
+  exit
+fi
+
+TARGET=$1
+
 source ./env.sh 
 
 echo "****************************"
-echo "clean hbase logs";
+echo "copy hbase logs";
 echo "****************************"
 
 for i in `cat $HBASE_CONF_DIR/masters $HBASE_CONF_DIR/regionservers`; do
   echo $i;
-  ssh $SSH_ARGS $i "rm -rf /grid/0/var/log/hbase/*; rm -rf /var/log/hbase/*" ;
+  mkdir $TARGET
+  scp $SSH_ARGS -r root@$i:/var/log/hbase/* $TARGET/;
 done
