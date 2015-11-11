277b131d060f22f21c3f8b23a8061c27ef9bf70b
Enis Soztutar <enis@apache.org>
2015-11-11 12:02:28 -0800

    dont copy jaas files

--- copy-conf.sh
+++ copy-conf.sh
@@ -15,5 +15,10 @@ echo "Copying hbase configuration";
 echo "****************************"
 for i in `cat /etc/hbase/conf/masters /etc/hbase/conf/regionservers /etc/hbase/conf/clients`; do
   echo $i;
-  scp $SSH_ARGS -r /etc/hbase/conf/* root@$i:/etc/hbase/conf/ ;
+  # we cannot copy everying under conf since jaas conf is per regionserver
+  scp $SSH_ARGS -r /etc/hbase/conf/hbase-site.xml root@$i:/etc/hbase/conf/ ;
+  scp $SSH_ARGS -r /etc/hbase/conf/hbase-env.xml root@$i:/etc/hbase/conf/ ;
+  scp $SSH_ARGS -r /etc/hbase/conf/masters root@$i:/etc/hbase/conf/ ;
+  scp $SSH_ARGS -r /etc/hbase/conf/regionservers root@$i:/etc/hbase/conf/ ;
+  scp $SSH_ARGS -r /etc/hbase/conf/log4j.properties root@$i:/etc/hbase/conf/ ;
 done
